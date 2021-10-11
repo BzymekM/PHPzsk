c42091cc6fc1f6116a8112f79d962c8e4eb0fc6e
BzymekM <mateusz.bzymek@uczen.zsk.poznan.pl>
2021-10-11 22:54:10 +0200

    10.10.2021

--- square.php
+++ square.php
@@ -7,8 +7,27 @@
   <body>
     <h3>Kwadrat</h3>
     <form method="get">
-      <input type="text" name="sideA" placeholder="Podaj długość boku a"> <br><br>
-      <input type="submit" value="Oblicz">
+      <input type="text" name="sideA" placeholder="Podaj długość boku a"><br><br>
+      <input type="submit" name="button" value="Oblicz">
     </form>
+    <?php
+      // if (!empty($_GET['sideA']) && is_numeric($_GET['sideA'])) {
+      if (!empty($_GET['sideA'])) {
+         //echo $_GET['sideA']."<br>";
+        $sideA=str_replace(',', '.', $_GET['sideA']); //str_replace(zbiera argument ze stringa, zamienia go na inny, wypisuje po zmianie)
+         //echo $sideA;
+        // pole
+        $area=$sideA**2;
+        // obwód
+        $circuit=4*$sideA;
+        echo <<< RESULT
+          <hr>
+          Pole kwadratu wynosi $area cm<sup>2</sup><br>
+          Obwód kwadratu wynosi $circuit cm
+RESULT;
+}else if (isset($_GET['button'])){
+        echo "Wypełnij wszystkie pola!";
+      }
+     ?>
   </body>
 </html>
